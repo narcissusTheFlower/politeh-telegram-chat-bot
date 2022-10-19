32dc53eba60e87cd613666026a6921dbc3804cfd
tseby <noviykomp13@mail.ru>
2022-10-19 21:21:52 +0300

    days left refactor

--- src/main/java/org/lev/utils/DaysTillSessionCounter.java
+++ src/main/java/org/lev/utils/DaysTillSessionCounter.java
@@ -13,7 +13,12 @@ public class DaysTillSessionCounter {
     public static String DaysTillSessionWithProperCase(){
         if (getDaysTillSession().endsWith(String.valueOf(1))) {
             return "остался " + getDaysTillSession() + " день";
-        }else return "осталось " + getDaysTillSession() + " дней";
+        }else if (getDaysTillSession().endsWith(String.valueOf(2)) ||
+                getDaysTillSession().endsWith(String.valueOf(3)) ||
+                getDaysTillSession().endsWith(String.valueOf(4))){
+            return "осталось " + getDaysTillSession() + " дня";
+        }else  return "осталось " + getDaysTillSession() + " дней";
+
     }
 
     private static String getDaysTillSession(){
